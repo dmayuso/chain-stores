3fc7d174ba75523ecf0319f9c3907332018a9cde
Daniel Ayuso <daniel.ayuso@innocv.com>
2018-01-03 19:18:24 +0100

    add service as provider on shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
+import {DataService} from "./data.service";
 
 @NgModule({
   declarations: [
@@ -16,6 +17,6 @@ import { HeaderComponent } from './header/header.component';
     FormsModule,
     HeaderComponent
   ],
-  providers: []
+  providers: [DataService]
 })
 export class SharedModule { }
