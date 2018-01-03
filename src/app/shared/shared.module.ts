80a0ed180b4a756a8584459192f54a38fcc68dcd
Daniel Ayuso <daniel.ayuso@innocv.com>
2018-01-03 18:53:16 +0100

    create shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    HeaderComponent
+  ],
+  providers: []
+})
+export class SharedModule { }
