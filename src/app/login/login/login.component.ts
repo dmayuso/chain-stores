2477d8f0f8c88e1f2433f994377c054696131a95
Daniel Ayuso <daniel.ayuso@innocv.com>
2018-05-29 09:56:43 +0200

    create build form and validators

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,6 +1,7 @@
 import {Component, HostListener, OnInit} from '@angular/core';
 import {AuthService} from "../../shared/services/auth.service";
 import {User} from "../../shared/domain/login.model";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 const ENTER_KEY_CODE = 13;
 
@@ -12,9 +13,23 @@ const ENTER_KEY_CODE = 13;
 export class LoginComponent implements OnInit {
 
   private user: User;
+  loginForm: FormGroup;
 
-  constructor(private authService: AuthService){
+  constructor(private authService: AuthService,
+              private formBuilder: FormBuilder){
+    this.buildForm();
     this.user = new User();
+  }
+
+  buildForm() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required, Validators.email, this.customValidator],
+      password: ['', Validators.minLength(8)]
+    })
+  }
+
+  customValidator(formControl: FormControl) {
+    return null;
   }
 
   ngOnInit() {
