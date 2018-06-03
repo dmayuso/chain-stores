08311577cc7712dc80d8e29194ab0df414813b40
Daniel Ayuso <daniel.ayuso@innocv.com>
2018-06-03 11:50:49 +0200

    create post method on data service

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -1,9 +1,18 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs/Observable";
 import { environment } from '../../../environments/environment';
 
 const apiUrl: string = `${environment.api}`;
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Cache-control': 'no-cache',
+    'Pragma': 'no-cache',
+    'Expires': '0'
+  })
+};
 
 @Injectable()
 export class DataService {
@@ -12,6 +21,10 @@ export class DataService {
 
   get(url: string): Observable<any>{
     return this.http.get(apiUrl.concat(url));
+  }
+
+  post(url: string, data: any) {
+    return this.http.post(apiUrl.concat(url), data, httpOptions);
   }
 
 }
